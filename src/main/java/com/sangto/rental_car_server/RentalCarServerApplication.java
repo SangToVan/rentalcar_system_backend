497b47f89e5c36af17a68629bb4a5bc584a99f2c
Sang <sangtovanzzz@gmail.com>
2025-01-20 20:18:22 +0700

    Initial commit

---
+++ src/main/java/com/sangto/rental_car_server/RentalCarServerApplication.java
@@ -0,0 +1,13 @@
+package com.sangto.rental_car_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sangto/rental_car_server/RentalCarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangto.rental_car_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
